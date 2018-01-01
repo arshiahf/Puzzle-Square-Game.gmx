aa1423312cdfcff05f0f38e91f8bbbe523642329
MTaurone <arshiahf@gmail.com>
2018-01-01 02:10:37 -0800

    Added lib return function Created/added first hint by difficulty file No skeleton yet implemented

--- datafiles/puzzle_square_game/src/lib.rs
+++ datafiles/puzzle_square_game/src/lib.rs
@@ -5,3 +5,22 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+mod prime
+{
+    pub fn choose_difficulty(difficulty:str) -> str
+    {
+        let hint:str = match difficulty
+        {
+            "beginner" => pattern_beginner(),
+            "very_easy" => pattern_very_easy(),
+            "easy" => pattern_easy(),
+            "hard" => pattern_hard(),
+            "very_hard" => pattern_very_hard(),
+            "expert" => pattern_expert(),
+            _ => pattern_normal(),
+        }
+
+        hint
+    }
+}
